0b058d6bc09b24361541b392fb390130f5f4f185
Parker Catalano <catalano.work@gmail.com>
2017-11-03 17:10:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
   def initialize(name, artist, genre)
     @@count +=1
     @@artists << artist
-    @@genre << genre
+    @@genres << genre
     @artist = artist
     @genre = genre
     @name = name
